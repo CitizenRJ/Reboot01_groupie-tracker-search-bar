354c59a996ec8ca991ab49e4caa1fafaaa98e294
RJ <rami.jamal@me.com>
2024-05-09 06:22:59 +0300

    Final Countdown

--- internal/utils/isin.go
+++ internal/utils/isin.go
@@ -1,5 +1,8 @@
 package groupie
 
+// Isin checks if the given element is present in the provided slice of strings.
+// If the slice is nil, it returns false.
+// Otherwise, it iterates through the slice and returns true if the element is found, false otherwise.
 func Isin(element string, tab []string) bool {
 	if tab == nil {
 		return false

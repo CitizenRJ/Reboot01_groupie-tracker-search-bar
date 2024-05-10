3dfb9e47904be405573df4f658956a094720fa0c
hx03s <habdulla2903@gmail.com>
2024-05-10 11:21:26 +0300

    almost done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,201 +1,181 @@
 package main
 
 import (
-	"fmt"
-	gpi "groupie/internal/api"
-	gpd "groupie/internal/models"
-	gpf "groupie/internal/utils"
-	"html/template"
-	"net/http"
-	"path/filepath"
-	"runtime"
-	"strconv"
+    "fmt"
+    gpi "groupie/internal/api"
+    gpd "groupie/internal/models"
+    gpf "groupie/internal/utils"
+    "html/template"
+    "net/http"
+    "path/filepath"
+    "runtime"
+    "strconv"
 )
 
-// global variables that holds data from the GetData function.
 var allData gpd.Data
 var date gpd.Date
 var artists []gpd.Artists
 var getLocation gpd.GetLocation
 var relations gpd.Relations
 
-// main is the entry point for the server application. It sets up the necessary data, configures the HTTP server,
-// and starts listening for incoming requests on port 8080.
-// The main function performs the following tasks:
-// 1. Retrieves data from the gpi package and stores it in the allData variable.
-// 2. Prints a message indicating that the server is starting on localhost:8080.
-// 3. Registers HTTP handlers for the following routes:
-//   - "/" and "/home": Handles the index page.
-//   - "/search": Handles the search functionality.
-//   - "/filter": Handles the filtering functionality.
-//   - "/info": Handles the information page.
-//   - "/style.css": Serves the CSS file for the web application.
-//
-// 4. Starts the HTTP server and listens for incoming requests.
-// 5. If an error occurs while starting the server, it prints the error message and returns.
+type writerWrapper struct {
+    http.ResponseWriter
+    headerWritten bool
+}
+
+func (w *writerWrapper) WriteHeader(code int) {
+    if !w.headerWritten {
+        w.ResponseWriter.WriteHeader(code)
+        w.headerWritten = true
+    }
+}
+
+func (w *writerWrapper) Write(data []byte) (int, error) {
+    if !w.headerWritten {
+        w.WriteHeader(http.StatusOK)
+    }
+    return w.ResponseWriter.Write(data)
+}
+
 func main() {
-	date, artists, getLocation, relations = gpi.GetData()
-	allData = gpi.SetData(date, artists, getLocation, relations)
-	const port = ":8080"
-	fmt.Println("Starting server at http://localhost" + port)
-	http.HandleFunc("/", handleIndex)
-	http.HandleFunc("/home", handleIndex)
-	http.HandleFunc("/search", handleSearch)
-	http.HandleFunc("/filter", handleFilter)
-	http.HandleFunc("/info", handleInfos)
-	http.HandleFunc("/style.css", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../../web/templates/style.css")
-	})
-	err := http.ListenAndServe(port, nil)
-	if err != nil {
-		fmt.Printf("Error starting server: %v", err)
-		return
-	}
+    date, artists, getLocation, relations = gpi.GetData()
+    allData = gpi.SetData(date, artists, getLocation, relations)
+    const port = ":8080"
+    fmt.Println("Starting server at http://localhost" + port)
+    http.HandleFunc("/", handleIndex)
+    http.HandleFunc("/home", handleIndex)
+    http.HandleFunc("/search", handleSearch)
+    http.HandleFunc("/filter", handleFilter)
+    http.HandleFunc("/info", handleInfos)
+    http.HandleFunc("/style.css", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "../../web/templates/style.css")
+    })
+    err := http.ListenAndServe(port, nil)
+    if err != nil {
+        fmt.Printf("Error starting server: %v", err)
+        return
+    }
 }
 
-// handleIndex is an HTTP handler function that renders the home.html template with the allData
-// data structure. If there is an error parsing the template or executing it, the function
-// will return a 500 Internal Server Error response.
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-	var templateInstance *template.Template
-	templatePath, err := getTemplatePath("home.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	templateInstance = template.Must(template.ParseFiles(templatePath))
-	err = templateInstance.Execute(w, allData)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+    wrapper := &writerWrapper{ResponseWriter: w}
+    var templateInstance *template.Template
+    templatePath, err := getTemplatePath("home.html")
+    if err != nil {
+        http.Error(wrapper, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    templateInstance = template.Must(template.ParseFiles(templatePath))
+
+    err = templateInstance.Execute(wrapper, allData)
+    if err != nil {
+        http.Error(wrapper, err.Error(), http.StatusInternalServerError)
+        return
+    }
 }
 
-// handleSearch handles the search functionality for the application.
-// It takes an HTTP request with a "input" form value, parses the query,
-// and uses the gpf.SearchData function to retrieve a resultSet of matching
-// data. It then renders the "home.html" template with the resultSet.
-// If the resultSet is empty, it still renders the "home.html" template.
-// Any errors that occur during template parsing or execution are
-// returned as an HTTP 500 Internal Server Error.
+
 func handleSearch(w http.ResponseWriter, r *http.Request) {
-	query := r.FormValue("input")
-	parsedQuery, _ := strconv.Atoi(query)
-	resultSet := gpf.SearchData(query, parsedQuery, allData)
+    query := r.FormValue("input")
+    parsedQuery, _ := strconv.Atoi(query)
+    resultSet := gpf.SearchData(query, parsedQuery, allData)
 
-	var pageTemplate *template.Template
-	if len(resultSet.Artist) == 0 {
-		templatePath, err := getTemplatePath("home.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		pageTemplate = template.Must(template.ParseFiles(templatePath))
-	} else {
-		templatePath, err := getTemplatePath("home.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		pageTemplate = template.Must(template.ParseFiles(templatePath))
-	}
-	err := pageTemplate.Execute(w, resultSet)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+    var pageTemplate *template.Template
+    if len(resultSet.Artist) == 0 {
+        templatePath, err := getTemplatePath("home.html")
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        pageTemplate = template.Must(template.ParseFiles(templatePath))
+    } else {
+        templatePath, err := getTemplatePath("home.html")
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        pageTemplate = template.Must(template.ParseFiles(templatePath))
+    }
+    err := pageTemplate.Execute(w, resultSet)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 }
 
-// handleFilter is an HTTP handler function that processes filter parameters from a request and
-// generates a filtered data set, which is then rendered using a template.
-// The function extracts various filter parameters from the request, such as member buttons,
-// city, creation date, and album date. It then calls the gpf.FilterData function to generate
-// a filtered data set based on these parameters.
-// If the filtered data set is empty, the function renders the "home.html" template. Otherwise,
-// it renders the "home.html" template with the filtered data set.
-// The function returns an HTTP error if there is an issue parsing the request parameters or
-// rendering the template.
 func handleFilter(w http.ResponseWriter, r *http.Request) {
-	allMembersButton := r.FormValue("MemberAll")
-	cityFilter := r.FormValue("city")
-	memberButton1, _ := strconv.Atoi(r.FormValue("Member1"))
-	memberButton2, _ := strconv.Atoi(r.FormValue("Member2"))
-	memberButton3, _ := strconv.Atoi(r.FormValue("Member3"))
-	memberButton4, _ := strconv.Atoi(r.FormValue("Member4"))
-	memberButton5, _ := strconv.Atoi(r.FormValue("Member5"))
-	memberButton6, _ := strconv.Atoi(r.FormValue("Member6"))
-	memberButton7, _ := strconv.Atoi(r.FormValue("Member7"))
-	memberButton8, _ := strconv.Atoi(r.FormValue("Member8"))
-	creationDateFilter, _ := strconv.Atoi(r.FormValue("creationdate"))
-	albumDateFilter, _ := strconv.Atoi(r.FormValue("albumdate"))
+    allMembersButton := r.FormValue("MemberAll")
+    cityFilter := r.FormValue("city")
+    memberButton1, _ := strconv.Atoi(r.FormValue("Member1"))
+    memberButton2, _ := strconv.Atoi(r.FormValue("Member2"))
+    memberButton3, _ := strconv.Atoi(r.FormValue("Member3"))
+    memberButton4, _ := strconv.Atoi(r.FormValue("Member4"))
+    memberButton5, _ := strconv.Atoi(r.FormValue("Member5"))
+    memberButton6, _ := strconv.Atoi(r.FormValue("Member6"))
+    memberButton7, _ := strconv.Atoi(r.FormValue("Member7"))
+    memberButton8, _ := strconv.Atoi(r.FormValue("Member8"))
+    creationDateFilter, _ := strconv.Atoi(r.FormValue("creationdate"))
+    albumDateFilter, _ := strconv.Atoi(r.FormValue("albumdate"))
 
-	var tabButton []int
-	tabButton = append(tabButton, memberButton1, memberButton2, memberButton3, memberButton4, memberButton5, memberButton6, memberButton7, memberButton8)
+    var tabButton []int
+    tabButton = append(tabButton, memberButton1, memberButton2, memberButton3, memberButton4, memberButton5, memberButton6, memberButton7, memberButton8)
 
-	filteredData := gpf.FilterData(allData, allMembersButton, tabButton, cityFilter, creationDateFilter, albumDateFilter)
+    filteredData := gpf.FilterData(allData, allMembersButton, tabButton, cityFilter, creationDateFilter, albumDateFilter)
 
-	var templateInstance *template.Template
-	if len(filteredData.Artist) == 0 {
-		templatePath, err := getTemplatePath("home.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		templateInstance = template.Must(template.ParseFiles(templatePath))
-	} else {
-		templatePath, err := getTemplatePath("home.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		templateInstance = template.Must(template.ParseFiles(templatePath))
-	}
-	err := templateInstance.Execute(w, filteredData)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+    var templateInstance *template.Template
+    if len(filteredData.Artist) == 0 {
+        templatePath, err := getTemplatePath("home.html")
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        templateInstance = template.Must(template.ParseFiles(templatePath))
+    } else {
+        templatePath, err := getTemplatePath("home.html")
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        templateInstance = template.Must(template.ParseFiles(templatePath))
+    }
+    err := templateInstance.Execute(w, filteredData)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 }
 
-// handleInfos is an HTTP handler function that retrieves artist information and renders an HTML template with the information.
-// The function expects a request parameter "id" which is used to look up the artist information.
-// The artist information is then passed to an HTML template for rendering.
-// If there is an error retrieving the template path or rendering the template, the function will return a 500 Internal Server Error response.
 func handleInfos(w http.ResponseWriter, r *http.Request) {
-	identifier := r.FormValue("id")
-	artistIdentifier, _ := strconv.Atoi(identifier)
-	artistIdentifier = artistIdentifier - 1
+    identifier := r.FormValue("id")
+    artistIdentifier, _ := strconv.Atoi(identifier)
+    artistIdentifier = artistIdentifier - 1
 
-	artistInformation := gpf.InfoArtist(allData, artistIdentifier)
+    artistInformation := gpf.InfoArtist(allData, artistIdentifier)
 
-	var templatePointer *template.Template
-	templatePath, err := getTemplatePath("info.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	templatePointer = template.Must(template.ParseFiles(templatePath))
-	err = templatePointer.Execute(w, artistInformation)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+    var templatePointer *template.Template
+    templatePath, err := getTemplatePath("info.html")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    templatePointer = template.Must(template.ParseFiles(templatePath))
+    err = templatePointer.Execute(w, artistInformation)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 }
 
-// getTemplatePath returns the absolute path to the specified template file.
-// It determines the current directory of the calling code, and then constructs the
-// path to the template file relative to the current directory.
-// If there is an error resolving the absolute path, an error is returned.
 func getTemplatePath(templateFile string) (string, error) {
-	_, currentFile, _, ok := runtime.Caller(0)
-	if !ok {
-		return "", fmt.Errorf("failed to get current file path")
-	}
-	currentDir := filepath.Dir(currentFile)
-	templatePath := filepath.Join(currentDir, "../../web/templates", templateFile)
-	absolutePath, err := filepath.Abs(templatePath)
-	if err != nil {
-		return "", fmt.Errorf("failed to resolve absolute path: %v", err)
-	}
-	return absolutePath, nil
+    _, currentFile, _, ok := runtime.Caller(0)
+    if !ok {
+        return "", fmt.Errorf("failed to get current file path")
+    }
+    currentDir := filepath.Dir(currentFile)
+    templatePath := filepath.Join(currentDir, "../../web/templates", templateFile)
+    absolutePath, err := filepath.Abs(templatePath)
+    if err != nil {
+        return "", fmt.Errorf("failed to resolve absolute path: %v", err)
+    }
+    return absolutePath, nil
 }
--- internal/search/search.go
+++ internal/search/search.go
@@ -1,62 +1,49 @@
 package groupie
 
 import (
-	gpd "groupie/internal/models"
-	gpf "groupie/internal/utils"
-	"strconv"
+    gpd "groupie/internal/models"
+    "strconv"
+    "strings"
 )
 
-// GetAll retrieves a list of unique strings from the provided Data, including artist names, member names,
-// location positions, first albums, and creation dates. The strings are processed to replace spaces with
-// forward slashes.
 func GetAll(data gpd.Data) []string {
-	artistNames := []string{}
-	memberNames := []string{}
-	positions := []string{}
-	firstAlbums := []string{}
-	creationDates := []string{}
-	combinedList := []string{}
-	uniqueList := []string{}
+    artistNames := []string{}
+    memberNames := []string{}
+    positions := []string{}
+    firstAlbums := []string{}
+    creationDates := []string{}
+    combinedList := []string{}
+    uniqueMap := make(map[string]bool)
 
-	for _, artist := range data.Artist {
-		processedName := ""
-		for _, letter := range artist.Name {
-			if string(letter) == " " {
-				processedName += "/"
-			} else {
-				processedName += string(letter)
-			}
-		}
-		artistNames = append(artistNames, processedName)
-		firstAlbums = append(firstAlbums, artist.First_album)
-		creationDates = append(creationDates, strconv.Itoa(artist.Creation_date))
-		for _, member := range artist.Members {
-			processedMember := ""
-			for _, letter := range member {
-				if string(letter) == " " {
-					processedMember += "/"
-				} else {
-					processedMember += string(letter)
-				}
-			}
-			memberNames = append(memberNames, processedMember)
-		}
-	}
+    for _, artist := range data.Artist {
+        processedName := strings.Replace(artist.Name, " ", "/", -1)
+        artistNames = append(artistNames, processedName)
+        firstAlbums = append(firstAlbums, artist.First_album)
+        creationDates = append(creationDates, strconv.Itoa(artist.Creation_date))
+        for _, member := range artist.Members {
+            processedMember := strings.Replace(member, " ", "/", -1)
+            memberNames = append(memberNames, processedMember)
+        }
+    }
 
-	for _, location := range data.Location {
-		positions = append(positions, location.Locations...)
-	}
+    for _, location := range data.Location {
+        positions = append(positions, location.Locations...)
+    }
 
-	combinedList = append(combinedList, artistNames...)
-	combinedList = append(combinedList, memberNames...)
-	combinedList = append(combinedList, positions...)
-	combinedList = append(combinedList, firstAlbums...)
-	combinedList = append(combinedList, creationDates...)
-	for _, element := range combinedList {
-		if !gpf.Isin(element, uniqueList) {
-			uniqueList = append(uniqueList, element)
-		}
-	}
+    combinedList = append(combinedList, strings.Join(artistNames, ""))
+    combinedList = append(combinedList, strings.Join(memberNames, ""))
+    combinedList = append(combinedList, strings.Join(positions, ""))
+    combinedList = append(combinedList, firstAlbums...)
+    combinedList = append(combinedList, creationDates...)
 
-	return uniqueList
+    for _, element := range combinedList {
+        uniqueMap[element] = true
+    }
+
+    uniqueList := make([]string, 0, len(uniqueMap))
+    for element := range uniqueMap {
+        uniqueList = append(uniqueList, element)
+    }
+
+    return uniqueList
 }
